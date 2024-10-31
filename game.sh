fbc88e9da26ce6596b83131cac96c0234ba31340
SSaan82 <ssaanvi@smail.pembinatrails.ca>
2024-10-31 13:50:32 -0500

    created a new repo and copied all game files over

---
+++ game.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Welcome to the Ultimate Adventure Game!"
+echo "Choose a location to explore: forest, castle, or cave."
+read location
+if [ "$location" == "forest" ]; then
+  cat forest.txt
+elif [ "$location" == "castle" ]; then
+  cat castle.txt
+elif [ "$location" == "cave" ]; then
+  cat cave.txt
+  echo "But wait..."
+  cat monster.txt
+else
+  echo "Invalid location."
+fi
+
